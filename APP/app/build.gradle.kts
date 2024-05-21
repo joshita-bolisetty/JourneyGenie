bc5cb4bf7dee99b46d6f25cb661f22e1116565d4
Joshita Bolisetty <99278080+joshita-bolisetty@users.noreply.github.com>
2024-05-21 13:54:17 +0530

    Add files via upload

---
+++ APP/app/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.journeygenie"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.journeygenie"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility=JavaVersion.VERSION_1_8
+                targetCompatibility=JavaVersion.VERSION_1_8
+    }
+
+    // Set JVM target version for Kotlin compilation
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("com.mapbox.maps:android:11.3.0")
+    implementation ("com.squareup.okhttp3:okhttp:3.14.7")
+    //implementation ("com.mapbox.mapboxsdk:mapbox-android-plugin-annotation-v9:10.0.0")
+
+
+
+
+
+
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+   // implementation("androidx.annotation:annotation:1.7.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("androidx.core:core-ktx:+")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
